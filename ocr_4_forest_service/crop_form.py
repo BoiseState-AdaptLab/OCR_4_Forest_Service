bd6d5eaaac74be16be5bfe4b542abfdbf8cfa34e
FloCiaglia <floriana.ciaglia@gmail.com>
2021-03-15 20:34:04 -0600

    Cleaned the code up

--- ocr_4_forest_service/crop_form.py
+++ ocr_4_forest_service/crop_form.py
@@ -42,10 +42,6 @@ def main():
             cropped_img = crop(img, x, y, w, h)
             
 
-            #Need explanation about 32x32. window too small to see the image. 
-            #scale image to 32x32
-            #scaled_img = cv2.resize(cropped_img, (32,32), interpolation = cv2.INTER_AREA)
-
             #display the image to user
             if cropped_img is not None:
                 cv2.imshow(window_name, cropped_img)
@@ -54,18 +50,10 @@ def main():
                 print("no image has been read")
             
             i += 1
-            #filename += str(fileCount)
-            #cv2.imwrite(filename, scaled_img)
-            #fileCount += 1
-            #print("before 0")
             cv2.waitKey(0)  
-            #print("after 0")
             cv2.destroyAllWindows()
-            #print("after destroyAllWindows")
             cv2.destroyWindow(window_name)
-            #print("after window_name")
             cv2.waitKey(10)  
-            #print("we got to the end")
 
         jsonFile.close()
     exit(0)
@@ -78,6 +66,6 @@ def crop(image, x, y, w, h):
 
 #function to resize found on https://stackoverflow.com/questions/44650888/resize-an-image-without-distortion-opencv
 
-#driver function 
+#main function 
 if __name__ == '__main__':
     main()
