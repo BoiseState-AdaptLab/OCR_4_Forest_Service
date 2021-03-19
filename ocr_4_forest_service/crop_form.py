381ef09bfa75ec4bad2b6ad2e5f052c26773cf08
Isaac Bard <isaacbard@u.boisestate.edu>
2021-03-19 13:13:48 -0600

    Updated cropping form with proper field data

--- ocr_4_forest_service/crop_form.py
+++ ocr_4_forest_service/crop_form.py
@@ -1,4 +1,4 @@
-#Author: Floriana Ciaglia
+# Author: Floriana Ciaglia
 # Data: March 8, 2021
 # https://yangcha.github.io/iview/iview.html
 import cv2
@@ -7,69 +7,74 @@ import string
 import json
 import shutil
 
+
 def main():
     path = None
     window_name = "image"
     i = 0
-    #prompt user for input
-    if(path is not 'q'):
-        path  = input("Enter the path to the Forest Service Sheet or type q to quit: ")
-        
+    # prompt user for input
+    if (path is not 'q'):
+        path = input("Enter the path to the Forest Service Sheet or type q to quit: ")
 
         while not os.path.isfile(path):
-            
+
             if path == 'q':
                 exit(0)
             path = input("** The file could not be found ** \n Enter a valid path to the file: ")
-            
-        
-        #open file as an image
+
+        # open file as an image
         img = cv2.imread(path)
-        
-        #load the json file
-        jsonFile = open('data/fields.json')
-        fields = json.load(jsonFile)
-    
+
+        # load the json file
+        json_file = open('data/fields.json')
+        fields = json.load(json_file)
+
         #
-        for field in fields:
-           #print(field)
-            x = int(field["x"])
-            y = int(field["y"])
-            w = int(field["width"])
-            h = int(field["height"])
+        for i in fields['fields']:
+            # print(field)
+            y = (i['coord_pixel'][0])
+            x = (i['coord_pixel'][1])
+            h = i['height']
+            w = i['width']
 
+            # print('Field ', i['field'])
+            # print('Width ', i['width'])
+            # print('Height ', i['height'])
+            # print('Top Left Pixel ', i['coord_pixel'])
 
-            #cropping the image
+            # cropping the image
             cropped_img = crop(img, x, y, w, h)
-            
-            #display the image to user
+
+            # display the image to user
             if cropped_img is not None:
                 cv2.imshow(window_name, cropped_img)
                 cv2.imwrite('field_{:>02}.jpg'.format(i), cropped_img)
 
                 old_path = os.path.abspath('field_{:>02}.jpg'.format(i))
-                new_path = old_path[:-12] + "output/" 
+                new_path = old_path[:-12] + "output/"
                 shutil.move(old_path, new_path)
             else:
                 print("no image has been read")
-            
+
             i += 1
-            cv2.waitKey(0)  
+            cv2.waitKey(0)
             cv2.destroyAllWindows()
             cv2.destroyWindow(window_name)
-            cv2.waitKey(10)  
+            cv2.waitKey(10)
 
-        jsonFile.close()
+        json_file.close()
     exit(0)
 
-#cropping function
+
+# cropping function
 def crop(image, x, y, w, h):
-    cropped = image[y:y+h, x:x+w]
-    
+    cropped = image[y:y + h, x:x + w]
+
     return cropped
 
-#function to resize found on https://stackoverflow.com/questions/44650888/resize-an-image-without-distortion-opencv
 
-#main function 
+# function to resize found on https://stackoverflow.com/questions/44650888/resize-an-image-without-distortion-opencv
+
+# main function
 if __name__ == '__main__':
     main()
