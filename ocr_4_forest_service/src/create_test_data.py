8339d39cca0f9041deaa0ffbaf7b834e76c8d922
FloCiaglia <floriana.ciaglia@gmail.com>
2021-05-05 12:40:21 -0600

    cleaned up the repo

--- ocr_4_forest_service/src/create_test_data.py
+++ ocr_4_forest_service/src/create_test_data.py
@@ -13,28 +13,35 @@ from io import BytesIO
 from PIL import ImageFilter
 from PIL import ImageEnhance
 from IPython.display import display
-import os.path
+import os
 from os import path
+import json
 
 def main():
-    # image names
-    image_name = [('r_l.png', 'r'), ('o_l.png', 'O'), ('lower_case_s.png', 'S')]
-    # create a dictionary to map each letter/digit to a number 
     
     digits = ['0', '1','2','3','4','5','6','7','8','9', 'A','B','C','D','E',
                 'F','G','H','I','J','K','L','M','N','O', 'P','Q','R','S','T',
                 'U','V','W','X','Y', 'Z', 'a','b','d','e','f','g','h','n','q','r','t']
-  
-    label_dict = {}
+
+    f = open('testingData.json')
+    data = json.load(f)
+
+    class_dict= {}
+    data_dict = {}
     counter = 0
-    
+  
+    for entry in data['entries']:
+        data_dict[entry['file_name']] = entry['class']
+
     for dig in digits:
-        label_dict[dig]=counter
+        class_dict[dig]=counter
         counter = counter + 1
 
-    names = [name[0] for name in image_name]
-    labels = [label[1] for label in image_name]
-
+    names = [name for name in data_dict]
+    labels = [label for label in class_dict]
+   
+    # open and clean the cvs file up so
+    # that is it ready for a fresh set of data points
     file = open("test_data.csv","r+")
     file.truncate(0)
     file.close()
@@ -43,31 +50,38 @@ def main():
     #iterate through each image in images/
     for image in names:
         # read the image in
-        my_path = "images/" + image
+        my_path = "preprocessed/" + image
         original = cv.imread(my_path,0)
-        
-        #binary thresholding takes the image to balck and white
-        ret, binaryThresh = cv.threshold(original,127,255,cv.THRESH_BINARY_INV)
+        if original is None:
+          print("Cannot find the image")
         
         # make sure it's the right shape
-        img = binaryThresh.reshape(28,28)
+        img = original.reshape(28,28)
+
+        # we need to save the image using 
+        # the pillow library to iterate through
+        # its pixels
         im = Image.fromarray(img)
+        #new_name = "images/" + image
+        #im.save(new_name, 'JPEG')
+        #img = Image.open(new_name)
         
-        new_name = "images/" + image
-        im.save(new_name, 'JPEG')
-     
-        img = Image.open(new_name)
-        
+        #At this point, the image is ready
+        #list of RGB values for each pixel in the image
         pix = []
-        
-        pix.append(label_dict[labels[location]])
+        #classification in number
+        classi = data_dict[image]
+        numeric_class = class_dict[classi]
+   
+        #the first column in the csv file is the classification
+        pix.append(numeric_class)
+        #pix.append(label_dict[labels[location]])
      
 
         #iterate through each pixel in the image and store its RBG value into a list
         for x in range(28):
             for y in range(28):
-                rbg_val = img.getpixel((x,y))
-                
+                rbg_val = im.getpixel((x,y))
                 pix.append(rbg_val)
       
         with open('test_data.csv', 'a', newline='') as file:
