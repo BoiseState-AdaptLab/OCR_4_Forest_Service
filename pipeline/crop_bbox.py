a79e81789faa696906177d313427976508643d20
FloCiaglia <floriana.ciaglia@gmail.com>
2021-05-28 16:21:02 +0200

    Cleaned up code and checked its functionality

--- pipeline/crop_bbox.py
+++ pipeline/crop_bbox.py
@@ -5,38 +5,65 @@
 #   the bounding boxes out of the form.
 import cv2
 import os
-import string
 import json
 import shutil
 
 
 def main():
-    path = None
-    window_name = "image"
-    i = 0
-   
-    dir = 'output/'
+    # clean the directory from previous data
+    # or create it
+    output_dir()
 
+    # open json file bbox_coord.json with bbox coordinates
+    fields = open_json()
+  
+    # iterate through fields and find the bboxes
+    crop(fields)
+
+    # the new images generated are stored in the single_chars/ directory
+        
+    exit(0)
+
+"""
+Load the data from the bbox coord file
+"""
+def open_json():
+    # load the json file
+    json_file = open('bbox_coord.json')
+    fields = json.load(json_file)
+    return fields
+
+
+"""
+ if the output dir already exists, it gets
+ cleaned up. Otherwise, it gets created 
+"""
+def output_dir():
+    # if it already exists, clean it
     if os.path.exists('single_chars'):
         try:
             shutil.rmtree('single_chars')
         except:
-            print("Didn't work")
-
+            print("Error: couldn't clean the single_chars/ directory")
+    # else, create it
     if not os.path.exists('single_chars'):
         os.makedirs('single_chars')
 
-    # load the json file
-    json_file = open('bbox_coord.json')
-    fields = json.load(json_file)
-  
+
+"""
+Iterates through fields and marks the min and max of bbox
+around a character
+"""
+def crop(fields):
+    DIR = 'fields/'
+    i = 0
+
     for dict in fields:
      
-        key = dict.keys()
         # we only get the key
-        new_d = [str(key) for key in key]
+        new_d = [str(key) for key in dict.keys()]
        
-        field_name = dir + new_d[0]
+        field_name = DIR + new_d[0]
 
         # open form as an image
         img = cv2.imread(field_name)
@@ -50,11 +77,11 @@ def main():
                 h = box['h']
 
                 # cropping the image
-                cropped_img = crop(img, x, y, w, h)
+                cropped_img = img[y:y + h, x:x + w]
 
                 # display the image to user
                 if cropped_img is not None:
-                    cv2.imshow(window_name, cropped_img)
+                    # cv2.imshow(window_name, cropped_img)
                     cv2.imwrite('box_{num}.jpg'.format(num = i), cropped_img)
 
                     old_path = os.path.abspath('box_{num}.jpg'.format(num = i))
@@ -65,18 +92,11 @@ def main():
 
                 i = i + 1 
 
-            json_file.close()
-    exit(0)
 
 
-# cropping function
-def crop(image, x, y, w, h):
-    cropped = image[y:y + h, x:x + w]
-
-    return cropped
-
-
-# main function
+"""
+Definition of main function
+"""
 if __name__ == '__main__':
     main()
 
