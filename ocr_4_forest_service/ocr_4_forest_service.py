52fb2c877af6fac32c0a8cf5210a63fdb1974b10
FloCiaglia <floriana.ciaglia@gmail.com>
2021-07-13 12:45:05 -0700

    Added testing flag to command line argument parsing

--- ocr_4_forest_service/ocr_4_forest_service.py
+++ ocr_4_forest_service/ocr_4_forest_service.py
@@ -1,6 +1,6 @@
 # Author: Floriana Ciaglia
 # Date: July 12, 2020
-# This file will run the entire pipeline
+# This file will run the production pipeline
 
 from src.crop_fields import crop_fields
 from src.form_alignment import form_alignment
@@ -9,6 +9,7 @@ from src.char_detection import char_detection
 from src.img_preprocessing import img_preprocessing
 from src.create_csv import create_csv
 import argparse
+import sys
 
 def main():
 
@@ -20,43 +21,54 @@ def main():
         help="path to JSON file with fields coordinates")
     ap.add_argument("-temp", "--template", type=str, required=True,
         help="path to template form for form alignment")
+    ap.add_argument("-t",  action='store_true',
+        help="Testing pipeline execution flag. If not specified, the program executes in production mode")
     args = vars(ap.parse_args())
 
+    print("len of args: ", type(args))
     # the main functions are called in order of execution
     # determined by the pipeline
 
-    # 1) the first step consists in aligning the form
-    # we want to process with one of our templates in 
-    # order to be able to work with know xy-coordinates 
-    form_alignment(args)
+    if args['t'] is False:
+        # 1) the first step consists in aligning the form
+        # we want to process with one of our templates in 
+        # order to be able to work with know xy-coordinates 
+        form_alignment(args)
 
-    # 2) Once we have an aligned form, we crop each field
-    # box out of the image and generate the new images in
-    # a new directory 
-    crop_fields(args)
+        # 2) Once we have an aligned form, we crop each field
+        # box out of the image and generate the new images in
+        # a new directory 
+        crop_fields(args)
 
-    # 3) Now that we have field images, we  
-    # detect single characters in the fields 
-    # and generate a JSON file with the coordinates
-    # of the detected characters
-    char_detection()
+        # 3) Now that we have field images, we  
+        # detect single characters in the fields 
+        # and generate a JSON file with the coordinates
+        # of the detected characters
+        char_detection()
 
-    # 4) We read the newly generated JSON file and crop
-    # the single character images out of the fields and 
-    # store them into a new directory
-    crop_bbox()
+        # 4) We read the newly generated JSON file and crop
+        # the single character images out of the fields and 
+        # store them into a new directory
+        crop_bbox()
 
-    # 5) Finally, we pre process each single character
-    # image to minimize noise and refine the pen stroke.
-    
-    img_preprocessing()
+        # 5) Finally, we pre process each single character
+        # image to minimize noise and refine the pen stroke.
+        img_preprocessing()
 
-    # 6) This last step will generate a csv file with the RGB 
-    # value of each pixel in the image - this csv file will
-    # be the input for the Optical Character Recognition model. 
-    create_csv()
-    
-    print("Done Executing Production Pipeline!")
+        # 6) This last step will generate a csv file with the RGB 
+        # value of each pixel in the image - this csv file will
+        # be the input for the Optical Character Recognition model. 
+        create_csv()
+        
+        print("Done Executing Production Pipeline!")
+        
+    else: # The program is being run in testing mode.
+          # The execution will be similar to the 
+          # production mode except for classifications. 
+        print("we are in testing mode")
+        
+
+        
 
 
 #main function
