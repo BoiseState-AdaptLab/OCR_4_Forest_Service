160650697a6a78dc5865ead934755192917e4fc8
FloCiaglia <floriana.ciaglia@gmail.com>
2021-07-14 14:53:46 -0700

    Merged two versions of char_detection script: the merged version contains both character detection and word segmentation

--- ocr_4_forest_service/src/char_detection.py
+++ ocr_4_forest_service/src/char_detection.py
@@ -15,13 +15,15 @@ import os.path
 from os import path
 from PIL import Image
 from queue import LifoQueue
+import numpy as np
 
 """
 Definition of the main fuction
 """
 def char_detection():
     img_preprocess()
-    
+    json_file = word_segmentation()
+    create_json(json_file)
   
 
 """
@@ -154,7 +156,7 @@ Creates the json file with the bbox coordinates
 def create_json(list_of_dict):
     
     # store the data into a new json file
-    with open("bbox_coord.json", 'a') as outfile: 
+    with open("bbox_coord.json", 'w') as outfile: 
         json.dump(list_of_dict, outfile)
     
 
@@ -259,3 +261,262 @@ def check_coord(x, y, max_x, min_x, max_y, min_y):
   return max_x, min_x, max_y,  min_y
 
 
+def word_segmentation():
+
+    # load the json file
+    json_file = open('bbox_coord.json')
+    letters = json.load(json_file)
+    json_list = {}
+    json_list = []
+
+    for fields in letters:
+        characters = {}
+        char_points = []
+        path = 'fields/' + list(fields.keys())[0]
+        field_img = cv2.imread(path, 1)
+        # print("fields : ", fields)
+        
+        for field_name, dicts in fields.items():
+            # print("dicts : ", dicts)
+             
+            for box in dicts:
+                # print("box: ", box)
+                
+                x = int(box['x'])
+                y = int(box['y'])
+                w = int(box['w'])
+                h = int(box['h'])
+                word_img = field_img[y:y+h, x:x+w]
+
+                if word_img is None:
+                    print("we had an issue reading the image ")
+
+                # How many times does the height fits into the width?
+                ratio = w / h 
+                ratio_constrain=1.5
+                width_constrain=40
+                block_size=10
+                step_size=5
+                thresh=10
+                # Condition to run the slicing character approach
+                if ratio > ratio_constrain and w > width_constrain:
+                    # Performs somre preprocessing to the image
+                    gray_img = cv2.cvtColor(word_img, cv2.COLOR_BGR2GRAY)
+                    _, thresh_img = cv2.threshold(gray_img, -0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)
+                    # Remove long horizontal lines
+                    res_op_1 = cv2.morphologyEx(thresh_img, cv2.MORPH_OPEN, np.ones((1, 40), np.uint8))
+                    thresh_img -= res_op_1
+                    # Calculate the image vertical projection histogram
+                    word_v_hist_proj, word_h_hist_proj, v_hist_img, h_hist_img = create_h_v_image_proj(thresh_img)
+                    # Get characters
+                    if len(word_v_hist_proj) > block_size:
+                        seg_points = segmentation_points(word_v_hist_proj, block_size, step_size, thresh)
+                        # Improve segmentation positions
+                        seg_points = local_search(word_v_hist_proj, seg_points, block_size // 2)
+                        # Pixel transition count
+                        seg_points = pixel_transition_count(thresh_img, seg_points)
+                        # Update x coordinate in respect of original image
+                        seg_points = [int(char_x + x) for char_x in seg_points]
+                        # Keeps track of the original coordinates so then segmentation points
+                        # can be converted to coordinates
+                        if seg_points != []:
+                            char_points.append({"orig_coords": box, "char_coords": seg_points})
+                        else:
+                            char_points.append(box)
+                else:
+                    char_points.append(box)
+
+            # From segmentation lines to actual x, y, w, and h inside the field
+            bbox_coords = []
+            for chars in char_points:
+                if "orig_coords" in chars.keys():
+                    orig_x = chars['orig_coords']['x']
+                    orig_y = chars['orig_coords']['y']
+                    orig_h = chars['orig_coords']['h']
+                    orig_w = chars['orig_coords']['w']
+                    # Keeps track of the previous segmentation line x position 
+                    prev_seg_line = orig_x
+                    seg_lines = chars['char_coords']
+                    for seg_line in seg_lines:
+                        x = prev_seg_line
+                        w = seg_line - prev_seg_line
+                        y = orig_y
+                        h = orig_h
+                        bbox_coords.append({"x": x, "y": y, "w": w, "h": h})
+                        prev_seg_line = x + w
+                        # Append the last one, from the segmentation line to the end of the
+                        # field
+                    w = (orig_x + orig_w) - (x + w)
+                    bbox_coords.append({"x": prev_seg_line, "y": y, "w": w, "h": h})
+                else:
+                    bbox_coords.append(chars)
+                
+                # Remove single pixel width characters (too small to be a character) 
+                for coord in bbox_coords:
+                    w = coord['w']
+                    h = coord['h']
+                    if w <= 2 or h <= 2:
+                        bbox_coords.remove(coord)
+                # print(bbox_coords)
+
+            characters[field_name] = bbox_coords
+            # print("characters: ", characters)
+        # print(type(characters))
+        json_list.append(characters)
+        # print("list: ", json_list)
+            # print("bbox_coord at the end of the loop: ",bbox_coords)
+                    
+    return json_list
+
+
+
+def create_h_v_image_proj(thresh_img):
+    """Create vertical and horizontal histogram projections and their 
+    respective images.
+
+    Parameters:
+    thresh_img: 2D array that represent the pixel intensity value of an image
+    that has been thresholded (0 or 255 pixel values)
+
+    Returns:
+    word_v_hist_proj: 1D array same size as width of thresh_img where each 
+    element represents the white pixel frequency of one column of thresh_image
+    word_h_hist_proj: 1D array same size as height of thresh_img where each 
+    element represents the white pixel frequency of one row of thresh_image
+    v_hist_img: 2D array representing in an image the vertical histrogram 
+    projection
+    h_hist_img: 2D array representing in an image the horizontal histrogram 
+    projection
+    """
+    h, w = thresh_img.shape[:2]
+    # Creates black image same size as thresh_img
+    h_hist_img = np.zeros(thresh_img.shape[:2], dtype='uint8')
+    v_hist_img = np.zeros(thresh_img.shape[:2], dtype='uint8')
+    # Normalizes the image and counts pixels through rows and columns
+    word_v_hist_proj = np.sum(thresh_img / 255, axis=0)
+    word_h_hist_proj = np.sum(thresh_img / 255, axis=1) 
+    # Calcualtes the value of max pixel frequency in horizontal hist projection 
+    m = np.max(word_h_hist_proj)
+    for row in range(thresh_img.shape[0]):
+        # Creates a horizontal white line proportional size to the white pixel 
+        # frequency of that row
+        h_hist_img = cv2.line(
+            h_hist_img, 
+            (0,row), 
+            (int(word_h_hist_proj[row] * w/m),row), 
+            (255,255,255), 
+            1)
+    # Calculates the max pixel frequency value in vertical hist projection
+    m = np.max(word_v_hist_proj)
+    for col in range(thresh_img.shape[1]):
+        # Creates a horizontal white line proportional size to the white pixel
+        # frequency of that column
+        v_hist_img = cv2.line(
+        v_hist_img, 
+        (col,h), 
+        (col,h - int(word_v_hist_proj[col]*h/m)), 
+        (255,255,255),
+        1)
+    
+    return word_v_hist_proj, word_h_hist_proj, v_hist_img, h_hist_img
+
+
+def segmentation_points(v_projection, block_size, step_size, thresh):
+    """Algorithm that finds segmentation points in an image. Extracted from
+    section 4.3 of this paper: https://www.techscience.com/cmc/v60n1/28373 
+
+    Parameters: 
+      v_projection: 1D array with the vertical hist projection values
+      block_size: Size of the block to perform the sum 
+      step_size: Number of steps taking in each iteration
+      thresh: Minimum difference between blocks to be considered a segmentation
+      point
+
+    Returns:
+      seg: Array containing all x-axis segmentation points
+    """
+    seg = []
+    sumC = 0 # Sum of current block values
+    sumP = 0 # Sum of preceding block values
+    i = step_size
+    j = 0
+    k = 0
+    while k <= block_size:
+      sumP += v_projection[k]
+      k += 1
+    while i < (v_projection.shape[0] - block_size):
+      k = i
+      while k <= (i + block_size):
+        sumC += v_projection[k]
+        k += 1
+      if (sumP - sumC) > thresh:
+        # Keep the index of the segmentation
+        seg.insert(j, i + (block_size / 2)) 
+        j += 1
+        sumP = 0
+      else:
+        sumP = sumC
+      i += step_size
+      sumC = 0
+
+    return seg
+
+def local_search(v_hist, seg_points, length):
+    """Performs a local search around a segmentation point looking for the 
+    lowest pixel frequency value.
+
+    Parameters:
+      v_hist: 1D array representing the vertical histogram projection values
+      seg_points: Array containig all the x-axis segmentation points
+      length: Number to determine how far to look left and right of the
+      segmentation point
+
+    Returns:
+      to_ret: Array with updated segmentation points 
+    """
+    to_ret = []
+    for seg_p in seg_points:
+      start = int(seg_p - length)
+      end = int(seg_p + length + 1)
+      # Handle search out of bounds
+      if start < 0:
+        start = 0
+      if end > len(v_hist):
+        end = len(v_hist)
+      slice_to_check = v_hist[start:end]
+      # Gets the index of the lowest value of the slice and gets its true 
+      # position by adding the start position
+      to_ret.append(start + np.argmin(slice_to_check))
+    
+    return to_ret
+
+
+def pixel_transition_count(thresh_img, seg_points):
+    """Discard segmentation points that have more than two transitions from 
+    black to white.
+
+    Parameters:
+      thresh_img: 2D array representing the thresholded image (0 or 255 values)
+      seg_points: Array containing all the segmentations found
+    
+    Returns:
+      to_ret: Array with updated segmentation points 
+    """
+    to_ret = []
+    for seg_p in seg_points:
+      # Gets the pixel values vertically from the segmentation point position
+      v_slice = list(thresh_img[:,seg_p])
+      changes = 0
+      current = None
+      for val in v_slice:
+        if current == None:
+          current = val
+          continue
+        if val != current:
+          changes += 1
+        current = val
+      # Keep segmentaiton points that change on the vertical axis at max two times 
+      if changes <= 2:
+        to_ret.append(seg_p)
+    
+    return to_ret
