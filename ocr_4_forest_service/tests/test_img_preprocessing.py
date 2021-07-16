0364c9441a60bc3846199ca9989123e940c14f21
FloCiaglia <floriana.ciaglia@gmail.com>
2021-07-16 09:53:44 -0700

    Added test for image preprocessing

---
+++ ocr_4_forest_service/tests/test_img_preprocessing.py
@@ -0,0 +1,22 @@
+# this file contains functions that test the image preprocessing functionality
+
+from ..src.img_preprocessing import img_preprocessing
+import os
+
+def test_img_preprocessing():
+
+    img_preprocessing()
+
+    # assert that there are the same amount
+    # of images in the preprocessed/ dir as 
+    # in the single_chars/ dir
+
+    count_single = 0
+    count_prepro = 0
+    for img in os.listdir('single_chars/'):
+        count_single = count_single + 1
+    
+    for img in os.listdir('preprocessed/'):
+        count_prepro = count_prepro + 1
+    
+    assert count_single == count_prepro
