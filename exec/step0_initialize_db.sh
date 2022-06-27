362c6e102ed3e2b94869304ecc3c865c6e02914e
kdoster <77360073+kdoster@users.noreply.github.com>
2022-06-27 17:41:43 -0600

    Adjust psql directorys

--- exec/step0_initialize_db.sh
+++ exec/step0_initialize_db.sh
@@ -1,11 +1,11 @@
 # !/bin/bash
 
 # start the PostGres database server
-./../tools/postgresql-install/bin/pg_ctl -D ../forest_data start
+./../tools/bin/pg_ctl -D ../forest_data start
 echo "-- Started server"
 
 # run sql file to create all tables
-./../tools/postgresql-install/bin/psql -U flociaglia -d postgres -f db/sql-scripts/ForestServiceDatabaseINIT.sql
+./../tools/bin/psql -U flociaglia -d postgres -f db/sql-scripts/ForestServiceDatabaseINIT.sql
 echo "-- Report table and others created"
 
 # run SQLAlchemy scripts to create and populate validation tables
